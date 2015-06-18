983ee2e43d84683348b50d6861d0c2ceed5eae15
John Shearer <john@ultranaut.com>
2015-06-17 23:17:55 -0400

    Add a build task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,43 @@
 'use strict';
-
 var gulp = require('gulp');
+var del = require('del');
 var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
-
 var lint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-var config = {
-  jsFilesSrc: 'src/**/*.js'
-};
+var srcFile = 'src/*.js';
+
+gulp.task('clean', function (cb) {
+  del(['dist'], cb);
+});
 
 gulp.task('lint', function () {
-  gulp.src(config.jsFilesSrc)
-      .pipe(plumber())
-      .pipe(lint(config.jsHintRules))
-      .pipe(lint.format())
-      .pipe(lint.failOnError())
-      .on('error', notify.onError({ message: 'mistakes were made.' }));
+  // run as async so that minify won't run if there are errors
+  return gulp.src(srcFile)
+             .pipe(plumber())
+             .pipe(lint())
+             .pipe(lint.format())
+             .pipe(lint.failOnError());
+});
+
+gulp.task('minify', ['lint'], function () {
+  gulp.src(srcFile)
+      .pipe(uglify())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['lint', 'clean', 'minify'], function () {
+  gulp.src(srcFile)
+      .pipe(gulp.dest('dist/'));
 });
 
 /* --- watch ------------------------------------------------------- */
 gulp.task('watch', function () {
-  gulp.watch(config.jsFilesSrc, ['lint']);
-
+  gulp.watch(srcFile, ['lint']);
 });
 
 
 /* --- main -------------------------------------------------------- */
 gulp.task('default', ['lint', 'watch']);
-
-
